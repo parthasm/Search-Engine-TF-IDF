e8ba11b5df1185b01bfe9c77b573fbd847c21b6e
Partha Sarathi Mukherjee <parthasm2010@gmail.com>
2015-01-27 18:35:36 +0530

    Update Tokenizer.py
    converting to lower_case as it is the python convention

--- Tokenizer.py
+++ Tokenizer.py
@@ -1,28 +1,28 @@
 from nltk.corpus import stopwords
 import re
 
-sw = set(stopwords.words('english'))
+stopwords_english = set(stopwords.words('english'))
 
-def get_list_tokens_nltk(corpus, fileName):
-    string = get_raw_text(corpus,fileName)
+def get_list_tokens_nltk(corpus, file_name):
+    string = get_raw_text(corpus,file_name)
     return get_list_tokens_string(string)
 
 #!!!!!!!!------Possible Improvement: Stemming--------------#
   
 
 def get_list_tokens_string(string):
-    listWords = re.split(r'\W+',string)
-    return [w.lower() for w in listWords if w.isalpha() and len(w)>1 and w.lower() not in sw]    
+    list_words = re.split(r'\W+',string)
+    return [w.lower() for w in list_words if w.isalpha() and len(w)>1 and w.lower() not in stopwords_english]    
 
 
-def get_raw_text(corpus,fileName):
+def get_raw_text(corpus,file_name):
     string=''
     if corpus=='mr':
         from nltk.corpus import movie_reviews
-        string = movie_reviews.raw(fileids=fileName)
+        string = movie_reviews.raw(fileids=file_name)
     else:
         from nltk.corpus import reuters
-        string = reuters.raw(fileids=fileName)
+        string = reuters.raw(fileids=file_name)
     return string
 
 def get_list_fileids(corpus):
